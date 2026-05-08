04b77b3f3200208fbf4cd93ceb462bdd27a81ec2
sarradhibi <sarradhibi9834@gmail.com>
2026-05-08 15:24:24 +0100

    Week 3: Rendering Hacker News style lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
 import * as React from "react" ;
 
-let courseTitle= "advanced web dev" ;
+const stories = [ {
+  title: "React" ,
+  url: "https://reactjs.org/",
+  author: "John Doe",
+  objectID: 0,
+  points: 4,
+  num_comments: 3,
+},
+{
+  title: "Redux" ,
+  url: "https://reactjs.org/",
+  author: "Andrew Clark",
+  objectID: 2,
+  points: 5,
+  num_comments: 1,
+},];
 
 function App () {
-  const student = {
-  name : "sara" ,
-  age : 20 ,
-  track : "Business Analytics" ,
-};
-  return (
-    <div>
-      <p>{sayHello()}</p>
-      <h1>Hacker News</h1>
-      <p>welcome to {courseTitle}, {student.name}!</p>
-      <label htmlFor="search" >Search:</label>
-      <input id="search" type="text" ></input>
-    </div>
+  return(
+    <>
+      <h1>Hacker news</h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text"></input>
+      <ul>
+        {
+          stories.map(function (item){
+            return (
+              <li key={item.objectID} >
+                <span><a href={item.url}>{item.title}</a></span>
+                <span> {item.author}</span>
+                <span> {item.points}</span>
+                <span> {item.num_comments}</span>
+              </li>
+            )
+          }
+          )
+        }
+      </ul>
+
+    </>
+
   );
 }
-function sayHello () {
-  return "Hello!";
-}
-
 
 export default App; 
